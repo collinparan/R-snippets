02df97e5c2f9436ea5bf689f0bd8a3901b2e6e61
statsRcool <39640531+statsRcool@users.noreply.github.com>
2019-01-16 21:14:50 -0700

    Initial init
    GET and POST examples in R

---
+++ API_call_with_basic_auth.R
@@ -0,0 +1,21 @@
+library(httr)
+library(jsonlite)
+
+#Add your authorization keys below
+consumer_key = "KZMsb226rX5vJmzzB5fH";
+consumer_secret = "TXw7jGDNWdfjt7vvcVXQtCX0";
+auth_url = paste0("https://example.com/api/login?",
+                 "client_id=",consumer_key, 
+                 "&client_secret=", consumer_secret)
+
+#POST request for auth token
+req <- httr::POST(auth_url);
+raw_auth <- httr::content(req, as = "text")
+auth <- fromJSON(raw_auth)
+token <- auth$access_token
+
+#GET data
+url <- "https://tectonic.looker.com/api/"
+req <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", token, sep = " ")))
+json <- httr::content(req, as = "text")
+data <- fromJSON(json)
